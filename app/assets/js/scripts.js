719528239024b98fd519f04ea1abded1262603b5
SomilKumar <somil.kumar91@gmail.com>
2017-04-09 12:31:59 +0530

    setting up initial project

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * creative-template-basic
+ * 
+ * 
+ * @author somil
+ * @version 1.0.0
+ * Copyright 2017. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
